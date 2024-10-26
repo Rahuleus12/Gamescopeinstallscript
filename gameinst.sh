b147865e061e53a9a4c253b0d4ddeed0591b1de3
Geekfreak46 <58828810+Geekfreak46@users.noreply.github.com>
2024-10-26 16:29:44 +0530

    Add files via upload

---
+++ gameinst.sh
@@ -0,0 +1,3 @@
+#git clone https://github.com/ValveSoftware/gamescope.git
+git clone https://github.com/ChimeraOS/gamescope.git
+./gameup.sh
---
+++ gameup.sh
@@ -0,0 +1,6 @@
+cd gamescope
+git submodule update --init
+meson setup build/
+ninja -C build/
+#install line
+meson install -C build/ --skip-subprojects
