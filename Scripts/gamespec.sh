9bbfba027c9701eb490b906fbabbd29ae56d5175
Rahuleus12 <rajeshunique.51@gmail.com>
2025-03-03 01:03:09 +0530

    added a fix for the specific versions

--- Scripts/gamespec.sh
+++ Scripts/gamespec.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-id="db245f3";
-echo "Enter the hash of the gamescope version you wish to install (available from Gamescope Github)";
-read id;
+
+# Check if COMMIT_HASH is already set (passed from GSI-CLI.sh)
+if [ -z "$COMMIT_HASH" ]; then
+    # If not set, ask the user for input
+    read -p "Enter the commit hash from the official GitHub repository: " id
+else
+    # Use the hash passed from GSI-CLI.sh
+    id="$COMMIT_HASH"
+    echo "$id" >> hashHistory.txt
+    echo "Using commit hash: $id"
+fi
+
 git clone https://github.com/ValveSoftware/gamescope;
 cd gamescope;
 git checkout $id;
